657ef7ee414db77e3d05b7405cb04315dafd68ff
gustavo <gustavo26cs@gmail.com>
2020-09-14 03:04:41 -0300

    created routes and change pokemon class

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PokemonListComponent} from './pokemon-list/pokemon-list.component';
+import {PokemonFavoritesComponent} from './pokemon-favorites/pokemon-favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'pokemons', component: PokemonListComponent},
+  {path: '', redirectTo: 'pokemons', pathMatch: 'full'},
+  {path: 'favorites', component: PokemonFavoritesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pokemon.ts
+++ src/app/pokemon.ts
@@ -1,2 +1,12 @@
 export class Pokemon {
+  name: string;
+  url: string;
+  color: string;
+  entryNumber: number;
+  shape: string;
+  generation: string;
+  flavorText: string;
+  habitat: string;
+  image: string;
+  pokemonSpecies: any;
 }
