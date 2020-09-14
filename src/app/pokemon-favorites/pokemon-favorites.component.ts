e0fc9908d592f054ef2810a4d131688bd4c75f7d
gustavo <gustavo26cs@gmail.com>
2020-09-14 03:22:40 -0300

    created methods for pokemon-favorites component

--- src/app/pokemon-favorites/pokemon-favorites.component.ts
+++ src/app/pokemon-favorites/pokemon-favorites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Pokemon} from '../pokemon';
+import {PokemonService} from '../services/pokemon.service';
+import {StorageService} from '../services/storage.service';
 
 @Component({
   selector: 'app-pokemon-favorites',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonFavoritesComponent implements OnInit {
 
-  constructor() { }
+  pokemonSelected: Pokemon;
+  pokemonsFavorite: Pokemon[];
+  flagRemove: boolean;
+  private index: number;
+
+  constructor(private pokemonService: PokemonService, private storageService: StorageService) {
+    this.pokemonsFavorite = storageService.getData('pokemonStorage') || [];
+    this.pokemonSelected = new Pokemon();
+  }
 
   ngOnInit(): void {
+    this.getPokemon(this.pokemonsFavorite[0]);
+  }
+
+  getPokemon(data: Pokemon): any {
+    this.flagRemove = false;
+    this.pokemonSelected = data;
+  }
+
+  removeFavorite(pokemonSelected: Pokemon): any {
+    this.index = this.pokemonsFavorite.indexOf(pokemonSelected);
+    if (this.index !== -1) {
+      this.pokemonsFavorite.splice(this.index, 1);
+      this.storageService.setData('pokemonStorage', this.pokemonsFavorite);
+    }
+    this.index = undefined;
+    this.flagRemove = true;
   }
 
 }
