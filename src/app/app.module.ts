ea73eb08efaf60ab96b8c4b89abd17b4a6680d21
gustavo <gustavo26cs@gmail.com>
2020-09-14 02:57:29 -0300

    generate pokemon service and component pokemon-favorites

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonFavoritesComponent } from './pokemon-favorites/pokemon-favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent
+    PokemonListComponent,
+    PokemonFavoritesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pokemon-favorites/pokemon-favorites.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-favorites',
+  templateUrl: './pokemon-favorites.component.html',
+  styleUrls: ['./pokemon-favorites.component.css']
+})
+export class PokemonFavoritesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
